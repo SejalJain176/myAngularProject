0b42aea21387bfe5521cf0a8e96639b9841f3859
SejalJain176 <sejalj519@gmail.com>
2022-01-20 08:39:43 -0800

    commit

--- src/app/shared/star/star.component.ts
+++ src/app/shared/star/star.component.ts
@@ -1,16 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnChanges, OnInit, Output, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-star',
   templateUrl: './star.component.html',
   styleUrls: ['./star.component.css']
 })
-export class StarComponent implements OnInit {
+export class StarComponent implements OnInit, OnChanges {
 
   constructor() { }
 
   ngOnInit(): void {
   }
-rating=4;
-cropWidth=75;
+  @Input() rating:number= 0;
+  cropWidth:number=75;
+@Output() ratingClick: EventEmitter<string>=new EventEmitter<string>();
+  ngOnChanges(): void {
+    this.cropWidth = this.rating * 75 / 5;
+  }
+  onClick(){
+    this.ratingClick.emit(`the rating ${this.rating}`)
+  }
 }
