54c46e97e7fe3938f3292a8825061ea628a4cbcc
SejalJain176 <sejalj519@gmail.com>
2022-02-08 23:02:16 -0800

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,21 +2,37 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template:`
-  <ul class='nav-navbar-nav'>
-    <li><a [routerLink]="['/welcome']">Home</a></li>
-    <li><a [routerLink]="['/products']">Product List</a></li>
-  </ul>
+  template: `
+
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <a class="navbar-brand" href="#">My Site</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="#" [routerLink]="['/welcome']">Home <span class="sr-only">(current)</span></a>
+      <li class="nav-item active">
+        <a class="nav-link" href="#" [routerLink]="['/products']">Products </a>
+      </li>
+     
+     
+    </ul>
+    
+  </div>
+</nav>
   <div>
     <router-outlet></router-outlet>
-  </div>
-    `
-    ,
   
+    `
+  ,
+
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'myProject';
 
- 
+
 }
